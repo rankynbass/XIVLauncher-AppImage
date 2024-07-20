30bcb25e3b744d0e706f5f2de9c293408544c6ae
Rankyn Bass <rankyn@proton.me>
2024-07-20 13:15:32 -0700

    Fix install script

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ mkdir -p "$HOME"/Applications
 mkdir -p "$XDG_DATA_HOME"/icons/xivlauncher-core
 cp "$HERE"/XIVLauncher.Core-latest-x86_64.* "$HOME"/Applications/
 cp "$HERE"/xivlauncher.png "$XDG_DATA_HOME"/icons/xivlauncher-core/xivlauncher.png
-sed -i "s|Exec=|Exec=$HOME/Applications/XIVLauncher.Core-latest-x86_64.AppImage|" "$HERE"/XIVLauncher-AppImage.desktop
-sed -i "s|Icon=|Icon=$XDG_DATA_HOME/icons/xivlauncher-core/xivlauncher.png|" "$HERE"/XIVLauncher-AppImage.desktop
 cp "$HERE"/XIVLauncher-AppImage.desktop "$XDG_DATA_HOME"/applications/XIVLauncher-AppImage.desktop
+sed -i "s|Exec=|Exec=$HOME/Applications/XIVLauncher.Core-latest-x86_64.AppImage|" "$XDG_DATA_HOME"/applications/XIVLauncher-AppImage.desktop
+sed -i "s|Icon=|Icon=$XDG_DATA_HOME/icons/xivlauncher-core/xivlauncher.png|" "$XDG_DATA_HOME"/applications/XIVLauncher-AppImage.desktop
 xdg-desktop-menu forceupdate
