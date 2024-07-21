05f8857266f57cdea1cbe00922c4d429b4b717a6
Rankyn Bass <rankyn@proton.me>
2024-07-20 21:27:42 -0700

    Switch to static aria2 to make ipv6 work

--- make-appimage.sh
+++ make-appimage.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-XLCORE=https://github.com/rankynbass/XIVLauncher.Core/releases/latest/download/XIVLauncher.Core.tar.gz
-APPIMGBLDR=https://github.com/AppImageCrafters/appimage-builder/releases/download/v1.1.0/appimage-builder-1.1.0-x86_64.AppImage
-
-# Get XLCore
-curl -L "$XLCORE" -o XIVLauncher.Core.tar.gz
-rm -rf AppDir/*
-mkdir -p AppDir/opt/xivlauncher-core
-tar -xf XIVLauncher.Core.tar.gz -C AppDir/opt/xivlauncher-core
-mkdir -p AppDir/usr/share/icons/hicolor/256x256/apps
-cp xivlauncher.png AppDir/usr/share/icons/hicolor/256x256/apps/xivlauncher.png
-
-# Get appimage-builder
-curl -L $APPIMGBLDR -o appimage-builder
-chmod +x appimage-builder
 ./appimage-builder --skip-test
 
 # Package the artifact
