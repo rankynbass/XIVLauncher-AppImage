71d3e0c4d9965a46f2602939e0dcdf8ab04a3ea4
Rankyn Bass <rankyn@proton.me>
2026-06-24 13:46:29 -0700

    Add xdg-open wrapper

--- appimage-build/resources/xivlauncher.sh
+++ appimage-build/resources/xivlauncher.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 OLD_LIBRARY_SEARCH_PATH=$(ldconfig -v 2>/dev/null | grep -E '^/.*:' | cut -d: -f1 | tr '\n' ':' | sed 's/:$//')
+export OLD_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$APPDIR/usr/lib:$OLD_LIBRARY_SEARCH_PATH:$LD_LIBRARY_PATH
 
+export OLD_PATH=$PATH
 export PATH=$APPDIR/usr/bin:$PATH
 
 if [ ! -z "$WAYLAND_DISPLAY" ] && ( [ "$XDG_CURRENT_DESKTOP" == "GNOME" ] || [ "$XDG_CURRENT_DESKTOP" == "ubuntu:GNOME" ] ); then
